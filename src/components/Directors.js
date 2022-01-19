c5bf220902796c1d21f4db67ff3738c3c11a7503
Erik Luebbers <erluebbers@gmail.com>
2022-01-19 15:18:23 -0800

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,26 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const displayDirectors = directors.map(director => {
+    const movieList = director.movies.map(movie => {
+      return <li key={movie}>{movie}</li>
+    })
+
+    return (
+      <div key={director.name}>
+        <h2>Name: {director.name}</h2>
+        <p>Movies:</p>
+        <ul>{movieList}</ul>
+      </div>
+    )
+  })
+
+  return (
+  <div>
+    <h1>Directors Page</h1>
+    {displayDirectors}
+  </div>
+  );
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,27 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  const displayMovies = movies.map(movie => {
+    const genres = movie.genres.map(genre => {
+      return <li key={genre}>{genre} </li>
+    })
+
+    return (
+      <div key={movie.title}>
+        <h2>{movie.title}</h2>
+        <p>Time: {movie.time}</p>
+        <ul>{genres}</ul>
+      </div>
+    )
+  })
+
+  return (
+  <div>
+    <h1>Movies Page</h1>
+    {displayMovies}
+  </div>
+  );
 }
 
 export default Movies;
